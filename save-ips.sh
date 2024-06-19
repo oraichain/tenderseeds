c1e0a487f6e12fa69a54d531bbe7bca8ff6f9ea0
phutx <phu.tx@orai.io>
2024-06-19 14:31:19 +0700

    add docker-compose file for local

---
+++ save-ips.sh
@@ -0,0 +1,7 @@
+bash /root/seeds/show-ips.sh osmosis | tr -d '"' > /root/seeds/osmosis.txt
+bash /root/seeds/show-ips.sh cosmos | tr -d '"' > /root/seeds/cosmoshub.txt
+bash /root/seeds/show-ips.sh injective | tr -d '"' > /root/seeds/injective.txt
+bash /root/seeds/show-ips.sh dymension | tr -d '"' > /root/seeds/dymension.txt
+bash /root/seeds/show-ips.sh celestia | tr -d '"' > /root/seeds/celestia.txt
+bash /root/seeds/show-ips.sh oraichain | tr -d '"' > /root/seeds/oraichain.txt
+echo "DONE";
